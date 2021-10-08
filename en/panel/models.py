3feace922805e37da6bf50326d3f3ed508c106a8
MahyaR-KD <mahyarkakvand96@gmail.com>
2021-10-08 17:08:15 +0330

    ...

--- en/panel/models.py
+++ en/panel/models.py
@@ -12,7 +12,15 @@ class HomeSlider(models.Model):
     title = models.CharField(max_length=32)
     slide_image = models.ImageField(upload_to='panel/images/backgrounds/')
     slider_text = models.CharField(max_length=200)
-    button_text = models.CharField(max_length=32)
+    pub_date = models.DateTimeField(default=datetime.now, blank=True)
+
+    def __str__(self):
+        return self.title
+
+class Project(models.Model):
+    title = models.CharField(max_length=32)
+    header_image = models.ImageField(upload_to='panel/images/backgrounds/')
+    header_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField(default=datetime.now, blank=True)
 
     def __str__(self):
