0e1fe0169da71fed220406f6b27e4ffff9ac2386
MahyaR-KD <mahyarkakvand96@gmail.com>
2021-10-09 14:34:28 +0330

    ...

--- en/panel/admin.py
+++ en/panel/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import HomeSlider, Project, Plans, OurTeam
+from .models import HomeSlider, Project, Plans, OurTeam, WhoWeAre, WhatWeDo, WorkProcess, OurSkills
 
 admin.site.register(HomeSlider)
 
@@ -10,3 +10,11 @@ admin.site.register(Project)
 admin.site.register(Plans)
 
 admin.site.register(OurTeam)
+
+admin.site.register(WorkProcess)
+
+admin.site.register(WhatWeDo)
+
+admin.site.register(WhoWeAre)
+
+admin.site.register(OurSkills)
--- en/panel/models.py
+++ en/panel/models.py
@@ -27,11 +27,18 @@ class Project(models.Model):
         return self.title
 
 
+TYPE_CHOICES = (
+    ('M', 'Month'),
+    ('Y', 'Years'),
+)
+
+
 class Plans(models.Model):
     title = models.CharField(max_length=32)
     price = models.IntegerField()
     feature = models.TextField(blank=True)
-    month = models.CharField(max_length=32)
+    count = models.CharField(max_length=32)
+    count_type = models.CharField(max_length=1, choices=TYPE_CHOICES)
     pub_date = models.DateTimeField(default=datetime.now, blank=True)
 
     def __str__(self):
@@ -46,3 +53,38 @@ class OurTeam(models.Model):
 
     def __str__(self):
         return self.full_name
+
+
+class WhoWeAre(models.Model):
+    title = models.CharField(max_length=32)
+    text = models.TextField(blank=False)
+    Strategy = models.TextField(blank=False)
+    Development = models.TextField(blank=False)
+
+    def __str__(self):
+        return self.title
+
+
+class WhatWeDo(models.Model):
+    title = models.CharField(max_length=32)
+    text = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.title
+
+
+class WorkProcess(models.Model):
+    title = models.CharField(max_length=32)
+    text = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.title
+
+
+class OurSkills(models.Model):
+    title = models.CharField(max_length=32)
+    counter = models.CharField(max_length=32)
+    feature = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.title
