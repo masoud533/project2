29a4df5c6c01b119d749dcd32f3abb629598b4ca
ubuntu <ubuntu@example.com>
2021-10-23 14:47:46 +0100

    ...

--- Fa/extensions/utils.py
+++ Fa/extensions/utils.py
@@ -4,4 +4,5 @@ from . import jalali
 def jalali_converter(time):
     time_to_str = "{},{},{}".format(time.year, time.month, time.day)
     convert = jalali.Gregorian(time_to_str).persian_tuple()
-    return convert
+    
+    return convert[0]
